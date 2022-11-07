39e8f7d1b207d891eb3298bb1a64f4949b060e79
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-11-07 19:58:31 +0100

    Challenge quête PHP Upload en cours

--- form.php
+++ form.php
@@ -1,81 +1,75 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] === "POST"){ 
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $uploadDir = 'uploads/';
-    $uploadFile = $uploadDir . uniqid();
     $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+    $uploadFile = $uploadDir . uniqid() . "." . $extension;
     $authorizedExtensions = ['jpg', 'png', 'gif', 'webp'];
     $maxFileSize = 1000000;
 
-    if( (!in_array($extension, $authorizedExtensions))){
-        $errors[] = 'Veuillez sélectionner une image de type Jpg, Png, Gif ou Webp !';
+    if ((!in_array($extension, $authorizedExtensions))) {
+        $errors[] = 'Veuillez sélectionner une image de type JPG, PNG, GIF ou WEBP !';
     }
 
-    if( file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize)
-    {
-        $errors[] = "Votre fichier doit faire moins de 1M !";
+    if (file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize) {
+        $errors[] = "Veuillez choisir un fichier de moins de 1Mo !";
     }
-
-
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Image de profil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 </head>
-    <body>
-        <form method="post" enctype="multipart/form-data">
-            <div>
-                <label for="firstname">Firstname:</label>    
-                <input type="text" name="firstname" id="firstname" />
-            </div>
-            <div>
-                <label for="lastname">Lastname:</label>    
-                <input type="text" name="lastname" id="lastname" />
-            </div>
-            <div>
-                <label for="age">Age:</label>    
-                <input type="number" name="age" id="age" />
-            </div>
-            <div>
-                <label for="imageUpload">Upload an profile image</label>    
-                <input type="file" name="avatar" id="imageUpload" />
-            </div>
-            <div>
-                <button name="send">Send</button>
-            </div>
-        </form>
-<?php
-if($_SERVER['REQUEST_METHOD'] === "POST")
-{
-    if(!empty($errors))
-    {
-        echo "<div><ul>";
-        foreach ($errors as $error)
-        {
-            echo "<li>";
-            echo $error;
-            echo "</li>";
-        }
-        echo "</ul></div>";
-    }
-    else {
-        move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);?>
-        <div class="card" style="width: 18rem;">
-          <img src="<?=$uploadFile?>" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title"><?=$_POST['firstname'] . " " . $_POST['lastname']?></h5>
-                <p class="card-text"><?=$_POST['age']?> yo</p>
-                <a href="#" class="btn btn-primary">Delete picture</a>
-        </div>
-<?php }}; ?>
-    </body>
-</html>
 
+<body>
+    <form method="post" enctype="multipart/form-data">
+        <div>
+            <label for="firstname">Prénom:</label>
+            <input type="text" name="firstname" id="firstname" required />
+        </div>
+        <div>
+            <label for="lastname">Nom:</label>
+            <input type="text" name="lastname" id="lastname" required />
+        </div>
+        <div>
+            <label for="age">Age:</label>
+            <input type="number" name="age" id="age" required />
+        </div>
+        <div>
+            <label for="imageUpload">Choisir une image</label>
+            <input type="file" name="avatar" id="imageUpload" required />
+        </div>
+        <div>
+            <button name="send">Envoyer</button>
+        </div>
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+        if (!empty($errors)) {
+            echo "<div><ul>";
+            foreach ($errors as $error) {
+                echo "<li>";
+                echo $error;
+                echo "</li>";
+            }
+            echo "</ul></div>";
+        } else {
+            move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile); ?>
+            <div class="card" style="width: 18rem;">
+                <img src="<?= $uploadFile ?>" class="card-img-top" alt="image de profil">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $_POST['firstname'] . " " . $_POST['lastname'] ?></h5>
+                    <p class="card-text"><?= $_POST['age'] ?> ans</p>
+                </div>
+        <?php }
+    }; ?>
+</body>
+
+</html>
